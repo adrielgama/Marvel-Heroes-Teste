90bedcb66b564b8ac1f364fd6e0b0bba82181e3c
Adriel Gama <adrielgama@gmail.com>
2021-05-15 17:12:04 -0300

    alter const api.js exclude .env keys

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import md5 from "md5";
 
 //EXCLUIR AO ENVIAR O ARQUIVO FINAL
-const publicKey = "579271c0b6b760fb4dd78b3297efea5c";
-const privateKey = "84c7f3e400faee71994b1dd7b56a4117f098c8b8";
+// const publicKey = "";
+// const privateKey = "";
 
-// const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
-// const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
+const publicKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
+const privateKey = process.env.REACT_APP_MARVEL_PRIVATE_KEY;
 
 const ts = Number(new Date());
 const hash = md5(ts + privateKey + publicKey);
